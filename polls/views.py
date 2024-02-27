a0c0425b0a6a9db71ecb47d0d8ab35029d29cb07
unknown <ncslove@gmail.com>
2024-02-27 22:46:46 +0900

    second

--- polls/views.py
+++ polls/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world.테스트 You're at the 에렵다. polls index.")
+    return HttpResponse("Hello, world. 다시 한번 you're at the 에렵다. polls index.")
