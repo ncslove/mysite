9276099336bad48f1466979d967826fec58fc1f5
Meorosi <109118785+ncslove@users.noreply.github.com>
2024-03-06 22:08:20 +0900

    any change?

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,3 +128,13 @@ DATABASES = {
         "NAME": "mydatabase",
     }
 }
+
+INSTALLED_APPS = [
+    "polls.apps.PollsConfig",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+]
